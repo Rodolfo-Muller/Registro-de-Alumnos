e7b790ee623686a47eb10afa060b84257184d9b3
Rodolfo-Muller <ro_muller111@hotmail.com>
2021-01-28 00:41:42 -0300

    Agregar archivos de proyecto.

---
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,148 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var db = new AlumnoContent())
+            {
+                //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList(); //Muestra solo los de la lista mayores de 18años
+                return View(db.Alumno.ToList());
+            }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+           // AlumnoContent db = new AlumnoContent();
+            //Con esta lista pide desde la base de datos todos los alumnos mayores de 18años 
+           
+
+
+            //Devuelve los TODOS los Alumnos de la Lista (base de datos)
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //CREA UN TOKEN QUE VERIFICA QUE AL FORMULARIO QUE SE ESTÁ ENVIANDO ES AL QUE PERTENECE
+        public ActionResult Add(Alumno a)
+        {
+            //Si el modelo no es Válido
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                using (AlumnoContent db = new AlumnoContent())//al usar el using le digo que abra la conecxion y a la vez la cierre
+                {
+                    a.FechaRegistro = DateTime.Now;
+                    db.Alumno.Add(a);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("","Error al registrar Alumno - " +  ex.Message);
+                return View();
+            }
+            
+        }
+
+        public ActionResult Editar(int ID)
+        {
+            try
+            {
+                using (var db = new AlumnoContent())
+                {
+                    //Alumno al = db.Alumno.Where(a => a.ID == ID).SingleOrDefault(); //Siempre lo puedo usar.
+                    Alumno alu = db.Alumno.Find(ID); //solo lo voy a buscar cuando solo tenga 1 clave primaria.
+                    return View(alu);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Alumno a)
+        {
+            try
+            {
+                using (var db = new AlumnoContent())
+                {
+                    Alumno al = db.Alumno.Find(a.ID);
+                    al.Nombre = a.Nombre;
+                    al.Apellido = a.Apellido;
+                    al.Edad = a.Edad;
+                    al.Sexo = a.Sexo;
+
+                    //Guardar Cambios
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult Detallesalumno(int ID)
+        {
+            try
+            {
+            using (var db = new AlumnoContent())
+            {
+                Alumno alu = db.Alumno.Find(ID); 
+                return View(alu);
+            }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult EliminarAlumno(int ID)
+        {
+            try
+            {
+                using (var db = new AlumnoContent())
+                {
+                    Alumno alu = db.Alumno.Find(ID);
+                    db.Alumno.Remove(alu);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CRUD-Alumnos/Models/AlumnoCE.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Alumnos.Models
+{
+    public class AlumnoCE
+    {
+        [Required]
+        [Display(Name ="Ingrese Nombres: ")]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Apellidos: ")]
+        public string Apellido { get; set; }
+        [Required]
+        [Display(Name = "Edad del Alumno: ")]
+        public int Edad { get; set; }
+        [Required]
+        [Display(Name = "Sexo del Alumno: ")]
+        public string Sexo { get; set; }
+
+    }
+
+    [MetadataType(typeof(AlumnoCE))]
+    public partial class Alumno
+    {
+       // public int Estado { get; set; }
+        public string NombreCompleto { get { return Nombre + " " + Apellido; } }
+    }
+}
